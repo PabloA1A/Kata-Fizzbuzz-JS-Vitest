4eb690f9494aaad62efae34e2ec3473659dacbf9
Pilpod <giacomo.corso@gmail.com>
2023-06-08 22:08:14 +0200

    init repo

---
+++ tests/FizzBuzz.test.js
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+import FizzBuzz from '../src/FizzBuzz';
+
+describe.skip('FizzBuzz', () => {
+    
+    it.skip('if number divisible by 3 return Fizz', () => {
+        
+    });
+
+});
